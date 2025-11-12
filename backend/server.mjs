1620b9c6dd58f0ad10eab935f70d62b487c6c798
Viacheslav <viacheslav@gmail.com>
2025-11-13 01:36:16 +0200

    Fix CORS

--- backend/server.mjs
+++ backend/server.mjs
@@ -24,7 +24,11 @@ if (!MONGO) {
 }
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+}));
 app.use(express.json({ limit: "20mb" }));
 
 // ====== DATABASE CONNECTION ======
